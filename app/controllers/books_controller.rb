9876bc72997c23d28f83f97ff7b90a75ccb90287
EC2 Default User <ec2-user@ip-172-31-80-235.ec2.internal>
2022-01-26 06:19:31 +0000

    [finish]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,14 +10,20 @@ class BooksController < ApplicationController
   def show
     @new_book = Book.new
     @book = Book.find(params[:id])
-     @user = current_user
+     @user = @book.user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
+    if  @book.save
+      flash[:notice] = "You have created book successfully"
     redirect_to book_path(@book.id)
+    else
+      @user = current_user
+      @books = Book.all
+      render 'index'
+    end
   end
 
   def new
@@ -26,13 +32,34 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    @book.user = current_user
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy!
     redirect_to books_path
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice:  "You have updated book successfully"
+    else
+      render :edit
+    end
   end
 
   private
 
-  def book_params
+   def book_params
     params.require(:book).permit(:title, :body)
-  end
+   end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
     @book = Book.new
+    @books = @user.books.page(params[:page]).reverse_order
+
   end
 
   def index
@@ -22,10 +25,20 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:notice] = "You have updated user successfully"
-      redirect_to user_path(@user.id)
+      redirect_to user_path(@user.id), notice: "You have updated user successfully"
     else
+      flash[:notice] = "You have updated user successfully."
       render "edit"
+    end
+  end
+  def create
+      @book = Book.new(book_params)
+    if @book.save
+     flash[:notice] = "Book was successfully created."
+     redirect_to book_path(@book.id)
+    else
+     @books = Book.all
+     redirect_to books_path
     end
   end
 
